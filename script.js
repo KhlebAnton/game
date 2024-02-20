5b0e649f0e70fe65d5c0c2c380c77fc088d7484b
Anton Khlebnikov <toni.farsh@icloud.com>
2024-02-20 19:12:24 +0500

    1th commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const player = document.getElementById('player');
+const danger = document.getElementById('danger');
+
+document.addEventListener('keydown' ,() => {
+    jump()
+}) 
+document.addEventListener('click' ,() => {
+    jump();
+    console.log(parseInt(player.offsetTop))
+}) 
+
+function jump() {
+    if(player.classList.contains != 'jump') {
+        player.classList.add ("jump")
+    }
+    setTimeout( function() {
+        player.classList.remove ("jump")
+    },800) 
+}
+
+setInterval(() => {
+
+    if(danger.offsetLeft <= (player.offsetWidth - 50) && danger.offsetLeft > 20 && player.offsetTop > 120)  {
+        
+        alert('game over');
+        location.reload()
+    }
+}, 20);
